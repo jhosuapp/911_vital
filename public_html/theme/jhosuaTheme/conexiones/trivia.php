2f76a04b540c9ceb542bbee4be032fbd22144673
jhosuappp <joshua@150porciento.com>
2022-11-11 17:27:14 -0500

    fix bug when send data to backend

--- public_html/theme/jhosuaTheme/conexiones/trivia.php
+++ public_html/theme/jhosuaTheme/conexiones/trivia.php
@@ -10,7 +10,7 @@ $totalQuestions = $_POST['totalQuestions'];
 $points = $_POST['points'];
 
 
-if($primera_pregunta && $segunda_pregunta && $tercer_pregunta && $time && $totalQuestions && $points){
+if($primera_pregunta && $segunda_pregunta && $tercer_pregunta && $time){
     echo json_encode([$primera_pregunta, $segunda_pregunta, $tercer_pregunta, $time, $totalQuestions, $points]);
 
     $consulta = "INSERT INTO trivia_preguntas(primera_pregunta, segunda_pregunta, tercera_pregunta, tiempo, preguntasbien, puntos) VALUES ('$primera_pregunta', '$segunda_pregunta', '$tercer_pregunta', '$time', '$totalQuestions', '$points')";
